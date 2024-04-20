8771d97b32e3e27300871077b600e6cce8474792
cuiyunxiang <1007568349@qq.com>
2024-04-21 04:18:43 +0800

    asd

--- scripts/check.ts
+++ scripts/check.ts
@@ -5,7 +5,9 @@ import { $ } from 'zx'
 import { printObject } from './utils'
 
 // check type and stage
-await Promise.all([$`pnpm lint:stage`]).catch((out: ProcessOutput) => {
-  printObject(out)
-  throw new Error(out.stdout)
-})
+// await Promise.all([$`pnpm lint:stage`]).catch((out: ProcessOutput) => {
+//   printObject(out)
+//   throw new Error(out.stdout)
+// })
+
+await $`pnpm lint:stage`
